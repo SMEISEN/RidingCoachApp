ee262b7f0c570cf0bd4c969975bbde78a3ecbc8d
smeisen <meisenbacher.stefanweb.de>
2020-05-13 15:00:59 +0200

    added vue cli service

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from backend.src import app
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -7,9 +8,11 @@ if os.path.exists(dotenv_path):
 
 
 class Config(object):
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     APP_DIR = os.path.dirname(__file__)
     ROOT_DIR = os.path.dirname(APP_DIR)
     DIST_DIR = os.path.join(ROOT_DIR, 'dist')
+
+
+app.config.from_object('backend.config.Config')
