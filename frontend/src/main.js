fb82804c7c6074c3ed2ae8917ed458e87dc34430
smeisen <meisenbacher.stefanweb.de>
2020-05-15 17:55:30 +0200

    added vuetify css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/index';
 import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
 
