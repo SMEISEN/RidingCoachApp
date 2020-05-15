82483b436adb894cbd37e8cf654e9fc972f290a5
smeisen <meisenbacher.stefanweb.de>
2020-05-15 23:32:35 +0200

    added mdi and roboto fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="blue darken-4"
+      color="primary"
       dark
     >
       <div class="d-flex align-center">
@@ -29,7 +29,7 @@
 <script>
 
 export default {
-  name: 'app',
+  name: 'App',
 
   data: () => ({
     //
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
-import app from './App.vue';
-import router from './router/index';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+
+import App from './App.vue';
+import router from './router/index';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: (h) => h(app),
+  render: (h) => h(App),
 }).$mount('#app');
