aa2a8a91b3e21614ed044aa8571b6dd22f612005
SMEISEN <meisenbacher.stefan@web.de>
2020-05-15 18:09:48 +0200

    added vuetify css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App';
 import router from './router/index';
 import vuetify from './plugins/vuetify';
 import 'vuetify/dist/vuetify.min.css';
