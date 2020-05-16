178a5b390a1e3e2ab7970aa0119d5423253a3262
SMEISEN <meisenbacher.stefan@web.de>
2020-05-16 19:10:15 +0200

    added restplus api with swagger documentation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="blue darken-4"
+      color="primary"
       dark
     >
       <div class="d-flex align-center">
@@ -21,7 +21,7 @@
     </v-app-bar>
 
     <v-content>
-      <router-view/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
