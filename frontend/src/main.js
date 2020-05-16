abb969725bf77ca2eb89d28c931c1fcd09339ef6
smeisen <meisenbacher.stefanweb.de>
2020-05-16 09:16:02 +0200

    added mdi and roboto fonts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import vuetify from './plugins/vuetify';
+import vuetify from '@/plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
