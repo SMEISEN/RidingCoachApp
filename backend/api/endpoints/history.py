a82c5c09142061d7f482d358365ede5a27cb73c8
smeisen <meisenbacher.stefanweb.de>
2020-05-21 18:07:38 +0200

    corrected put definition, removed update of row id

--- backend/api/endpoints/history.py
+++ backend/api/endpoints/history.py
@@ -13,6 +13,9 @@ class HistoryCollection(Resource):
 
     @api.response(200, 'Maintenance history list successfully fetched.')
     def get(self):
+        """
+        Returns a list of all maintenance history posts.
+        """
         history_schema = HistorySchema()
         history = HistoryModel.query.all()
 
@@ -27,6 +30,9 @@ class HistoryCollection(Resource):
 
     @api.response(201, 'Maintenance history successfully added.')
     def post(self):
+        """
+        Adds a maintenance history posts.
+        """
         last_history = HistoryModel.query.order_by(HistoryModel.hist_id.desc()).first()
         if last_history is None:
             last_id = -1
@@ -47,3 +53,52 @@ class HistoryCollection(Resource):
         db.session.commit()
 
         return None, 201
+
+
+@ns.route('/<int:id_>')
+@api.response(404, 'Maintenance history post not found.')
+class HistoryItem(Resource):
+
+    @api.response(200, 'Maintenance history with id <int:id> successfully fetched.')
+    def get(self, id_):
+        """
+        Returns a maintenance history post.
+        """
+        history_schema = HistorySchema()
+        history_post = HistoryModel.query.filter(HistoryModel.hist_id == id_).one()
+
+        response = jsonify(history_schema.dump(history_post))
+        response.status_code = 200
+
+        return response
+
+    @api.response(204, 'Post successfully updated.')
+    def put(self, id_):
+        """
+        Updates a maintenance history post.
+        """
+        inserted_data = request.get_json()
+
+        maintenance_history = HistoryModel.query.filter(HistoryModel.hist_id == id_).one()
+        maintenance_history.category = inserted_data['category']
+        maintenance_history.name = inserted_data['name']
+        maintenance_history.hours = inserted_data['hours']
+        maintenance_history.comment = inserted_data['comment']
+        maintenance_history.date = datetime.utcnow()
+
+        db.session.add(maintenance_history)
+        db.session.commit()
+
+        return None, 204
+
+    @api.response(204, 'Post successfully deleted.')
+    def delete(self, id_):
+        """
+        Deletes maintenance history post.
+        """
+        maintenance_history = HistoryModel.query.filter(HistoryModel.hist_id == id_).one()
+
+        db.session.delete(maintenance_history)
+        db.session.commit()
+
+        return None, 204
