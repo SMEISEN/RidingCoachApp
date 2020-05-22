644f1d8557443998a3535cf71f182d5bba082069
smeisen <meisenbacher.stefanweb.de>
2020-05-22 12:52:22 +0200

    implemented date time picker

--- backend/database/models/history.py
+++ backend/database/models/history.py
@@ -4,14 +4,15 @@ from backend.database import db, ma
 
 class HistoryModel(db.Model):
     hist_id = db.Column(db.Integer, primary_key=True)
-    #
+
     category = db.Column(db.String(20), nullable=False)  # Motor, Anbauteile, etc.
     name = db.Column(db.String(100), nullable=False)  # Öl gewechselt, etc.
-    #
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     hours = db.Column(db.Float, nullable=False)  # 77.5 h
-    #
     comment = db.Column(db.Text, nullable=True)
+
+    datetime_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    datetime_last_modified = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    datetime_display = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"History('{self.category}', '{self.name}', '{self.date}')"
