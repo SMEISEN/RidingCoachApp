b96b968a7aa6794efba64406fb039025c92904b9
SMEISEN <meisenbacher.stefan@web.de>
2020-05-22 18:21:14 +0200

    implemented appbar with tabs and dummy components for dashboard and maintenance

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Dashboard component</div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Dashboard from '../components/Dashboard.vue';
+import Maintenance from '../components/Maintenance.vue';
 import History from '../components/History.vue';
 
 Vue.use(VueRouter);
@@ -9,38 +11,74 @@ export default new VueRouter({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/dashboard',
       alias: '/home',
-      name: 'History',
-      component: History,
+      name: 'Dashboard',
+      component: Dashboard,
       meta: {
-        title: 'Home Page - Example App',
+        title: 'Dashboard',
+        metaTags: [
+          {
+            name: 'Dashboard',
+            content: 'tbd',
+          },
+          {
+            property: 'og:tbd',
+            content: 'tbd',
+          },
+        ],
+      },
+    },
+    {
+      path: '/maintenance',
+      name: 'Maintenance',
+      component: Maintenance,
+      meta: {
+        title: 'Maintenance',
         metaTags: [
           {
             name: 'description',
-            content: 'The home page of our example app.',
+            content: 'tbd',
           },
           {
-            property: 'og:description',
-            content: 'The home page of our example app.',
+            property: 'og:tbd',
+            content: 'tbd',
+          },
+        ],
+      },
+    },
+    {
+      path: '/history',
+      name: 'History',
+      component: History,
+      meta: {
+        title: 'History',
+        metaTags: [
+          {
+            name: 'History',
+            content: 'tbd',
+          },
+          {
+            property: 'og:tbd',
+            content: 'tbd',
           },
         ],
       },
     },
     {
       path: '/index.html',
-      name: 'History',
-      component: History,
+      name: 'Dashboard',
+      component: Dashboard,
       meta: {
-        title: 'Home Page - Example App',
+        title: 'Dashboard',
         metaTags: [
           {
-            name: 'description',
-            content: 'The home page of our example app.',
+            name: 'Dashboard',
+            content: 'tbd',
           },
           {
-            property: 'og:description',
-            content: 'The home page of our example app.',
+            property: 'og:tbd',
+            content: 'tbd',
           },
         ],
       },
