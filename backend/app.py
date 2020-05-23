bcc090f3de38af6011f6c41500604c846323e703
SMEISEN <meisenbacher.stefan@web.de>
2020-05-23 13:46:39 +0200

    updated create_tables command, maintenance categories and names are loaded from json template

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 import os
+import json
+from datetime import datetime
 from flask import Flask, Blueprint, current_app, send_file
 from flask_cors import CORS
 from backend.api import api
 from backend.api.endpoints.maintenance import ns as maintenance_namespace
+from backend.api.endpoints.maintenance import MaintenanceModel
 from backend.api.endpoints.history import ns as history_namespace
 from backend.api.endpoints.bike import ns as bike_namespace
 from backend.config import Config
@@ -37,6 +40,22 @@ def index_client():
 @app.cli.command(name='create_tables')
 def create_tables():
     db.create_all()
+
+    with open('backend/database/maintenance_category_name_interval.json') as json_file:
+        maintenance_template = json.load(json_file)
+
+    for entry in maintenance_template.items():
+        new_maintenance = MaintenanceModel(
+            mtn_id=entry[0],
+            category=entry[1]['category'],
+            name=entry[1]['name'],
+            hours_interval=entry[1]['hours_interval'],
+            datetime_created=datetime.utcnow(),
+            datetime_last_modified=datetime.utcnow(),
+        )
+        db.session.add(new_maintenance)
+    db.session.commit()
+
     print('Database created!')
 
 
@@ -47,12 +66,12 @@ def drop_tables():
 
 
 @app.cli.command(name='clear_tables')
-def clear_tables(session):
+def clear_tables():
     meta = db.metadata
     for table in reversed(meta.sorted_tables):
         print(f"Cleared table {table}!")
-        session.execute(table.delete())
-    session.commit()
+        db.delete(table)
+    db.session.commit()
 
 
 app.cli.add_command(create_tables)
