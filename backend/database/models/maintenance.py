a2ee0194f46c6c348947692c4fef8106ad693020
smeisen <meisenbacher.stefanweb.de>
2020-05-23 13:48:16 +0200

    added missing end of line, and removed hours_left and status from MaintenanceModel, to be calculated by frontend, not backend

--- backend/database/models/maintenance.py
+++ backend/database/models/maintenance.py
@@ -3,16 +3,15 @@ from backend.database import db, ma
 
 
 class MaintenanceModel(db.Model):
-    # fixed
     mtn_id = db.Column(db.Integer, primary_key=True)
+
     category = db.Column(db.String(20), nullable=False)  # Motor, Anbauteile, etc.
     name = db.Column(db.String(100), unique=True, nullable=False)  # Öl gewechselt, etc.
     hours_interval = db.Column(db.Float, nullable=False)  # 5h, 10h, ...
-    # variable
-    date_last = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    hours_last = db.Column(db.Float, nullable=False)  # 77.5 h
-    hours_left = db.Column(db.Float, nullable=False)  # hours_now - hours_last
-    status = db.Column(db.Float, nullable=False)  # hours_left/hours_interval * 100 %
+    hours_last = db.Column(db.Float)  # 77.5 h
+
+    datetime_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    datetime_last_modified = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Maintainance('{self.category}', '{self.name}', '{self.mtn_id}')"
