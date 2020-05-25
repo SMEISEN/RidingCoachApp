c2c641eb78e240836c4608e97e7add7ddc92cb08
smeisen <meisenbacher.stefanweb.de>
2020-05-25 18:29:51 +0200

    implemented api endpoint for getting bike characteristics

--- backend/api/endpoints/bike.py
+++ backend/api/endpoints/bike.py
@@ -1,4 +1,5 @@
-from backend.database.models.bike import BikeModel
+from flask import jsonify
+from backend.database.models.bike import BikeModel, BikeSchema
 from flask_restplus import Resource
 from backend.api import api
 
@@ -8,9 +9,19 @@ ns = api.namespace('bike', description='Operations related to blog posts')
 @ns.route('/')
 class BikeResource(Resource):
 
+    @api.response(200, 'Maintenance work list successfully fetched.')
     def get(self):
-        bike = BikeModel.query.all()
-        pass
+        """
+        Returns a list the bike characteristics.
+        """
+        bike_schema = BikeSchema()
+        bike_data = BikeModel.query.first()
+        bike_data = bike_schema.dump(bike_data)
+
+        response = jsonify(bike_data)
+        response.status_code = 200
+
+        return response
 
     def post(self):
         pass
