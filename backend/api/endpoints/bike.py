46ee678adf1612af51695f51fb70d576a020dfbc
SMEISEN <meisenbacher.stefan@web.de>
2020-06-07 11:38:13 +0200

    implemented get, put and delete specific bike routes and extended bike columns for storing tire informations

--- backend/api/endpoints/bike.py
+++ backend/api/endpoints/bike.py
@@ -5,22 +5,31 @@ from backend.api import api
 from backend.database import db
 
 ns = api.namespace('bike', description='Operations related to blog posts')
+bike_schema = BikeSchema()
 
-post_bike_parameters = api.model('PostBikeParameters', {
+bike_parameters = api.model('BikeParameters', {
     "operating_hours":
         fields.Float(description="operating hours", required=True),
     "manufacturer":
         fields.String(description="bike manufacturer", required=True),
     "model":
         fields.String(description="bike model", required=True),
+    "year":
+        fields.Integer(description="bike year", required=True),
     "ccm":
         fields.Float(description="engine size", required=False, example={}),
     "stroke":
         fields.Float(description="engine stroke", required=False, default=None),
     "piston":
         fields.Float(description="engine piston", required=False, default=None),
-    "year":
-        fields.Integer(description="bike year", required=True),
+    "slick_front":
+        fields.Float(description="front slick tire", required=False, default=None),
+    "slick_rear":
+        fields.Float(description="rear slick tire", required=False, default=None),
+    "rain_front":
+        fields.Float(description="front rain tire", required=False, default=None),
+    "rain_rear":
+        fields.Float(description="rear rain tire", required=False, default=None),
     "setup":
         fields.Raw(description="bike suspension and engine setup", required=False, default=None),
 })
@@ -35,7 +44,6 @@ class BikeCollection(Resource):
         Returns a list the bike characteristics.
         """
 
-        bike_schema = BikeSchema()
         bike_all_entries = BikeModel.query.all()
 
         bike_list = []
@@ -47,7 +55,7 @@ class BikeCollection(Resource):
 
         return response
 
-    @api.expect(post_bike_parameters)
+    @api.expect(bike_parameters)
     @api.response(201, 'Bike successfully added.')
     def post(self):
         """
@@ -55,19 +63,85 @@ class BikeCollection(Resource):
         """
 
         inserted_data = request.get_json()
-        print(inserted_data)
+
         new_bike = BikeModel(
-            operating_hours=inserted_data['operating_hours'],
-            manufacturer=inserted_data['manufacturer'],
-            model=inserted_data['model'],
-            ccm=inserted_data['ccm'],
-            stroke=inserted_data['stroke'],
-            piston=inserted_data['piston'],
-            year=inserted_data['year'],
-            setup=inserted_data['setup'],
+            operating_hours=inserted_data.get('operating_hours'),
+            manufacturer=inserted_data.get('manufacturer'),
+            model=inserted_data.get('model'),
+            year=inserted_data.get('year'),
+            ccm=inserted_data.get('ccm'),
+            stroke=inserted_data.get('stroke'),
+            piston=inserted_data.get('piston'),
+            slick_front=inserted_data.get('slick_front'),
+            slick_rear=inserted_data.get('slick_rear'),
+            rain_front=inserted_data.get('rain_front'),
+            rain_rear=inserted_data.get('rain_rear'),
+            setup=inserted_data.get('setup'),
         )
 
         db.session.add(new_bike)
         db.session.commit()
 
         return None, 201
+
+
+@ns.route('/<string:id_>')
+@api.response(404, 'Bike not found.')
+class BikeItem(Resource):
+
+    @api.response(200, "Bike with requested id successfully fetched.")
+    def get(self, id_):
+        """
+        Returns a bike.
+        """
+
+        bike_entry = BikeModel.query.filter(BikeModel.bike_id == id_).one()
+
+        bike_data = bike_schema.dump(bike_entry)
+
+        response = jsonify(bike_data)
+        response.status_code = 200
+
+        return response
+
+    @api.expect(bike_parameters)
+    @api.response(204, "Bike with requested id successfully updated.")
+    def put(self, id_):
+        """
+        Updates a bike.
+        """
+
+        inserted_data = request.get_json()
+
+        bike_entry = BikeModel.query.filter(BikeModel.bike_id == id_).one()
+        bike_entry.operating_hours = inserted_data.get('operating_hours')
+        bike_entry.manufacturer = inserted_data.get('manufacturer')
+        bike_entry.model = inserted_data.get('model')
+        bike_entry.year = inserted_data.get('year')
+        bike_entry.ccm = inserted_data.get('ccm')
+        bike_entry.stroke = inserted_data.get('stroke')
+        bike_entry.piston = inserted_data.get('piston')
+        bike_entry.slick_front = inserted_data.get('slick_front')
+        bike_entry.slick_rear = inserted_data.get('slick_rear')
+        bike_entry.rain_front = inserted_data.get('rain_front')
+        bike_entry.rain_rear = inserted_data.get('rain_rear')
+        bike_entry.setup = inserted_data.get('setup')
+
+        db.session.add(bike_entry)
+        db.session.commit()
+
+        return None, 204
+
+    @api.response(204, "History entry with requested id successfully deleted.")
+    def delete(self, id_):
+        """
+        Deletes a bike.
+        """
+
+        bike_entry = BikeModel.query.filter(BikeModel.bike_id == id_).one()
+
+        db.session.delete(bike_entry)
+        db.session.commit()
+
+        return None, 204
+
--- backend/database/models/bike.py
+++ backend/database/models/bike.py
@@ -12,11 +12,14 @@ class BikeModel(db.Model):
 
     manufacturer = db.Column(db.String(15), nullable=False)
     model = db.Column(db.String(15), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
     ccm = db.Column(db.Float, nullable=True)
     stroke = db.Column(db.Float, nullable=True)
     piston = db.Column(db.Float, nullable=True)
-    year = db.Column(db.Integer, nullable=False)
-
+    slick_front = db.Column(db.String(50), nullable=True)
+    slick_rear = db.Column(db.String(50), nullable=True)
+    rain_front = db.Column(db.String(50), nullable=True)
+    rain_rear = db.Column(db.String(50), nullable=True)
     setup = db.Column(JSON, nullable=True)
 
     def __repr__(self):
