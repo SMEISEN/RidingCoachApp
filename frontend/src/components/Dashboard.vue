0364f5c9fe98d4edc8fe9a7a167bfc90b41bfbe7
SMEISEN <meisenbacher.stefan@web.de>
2020-06-08 22:44:15 +0200

    implemented spare parts placeholder

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -98,6 +98,23 @@
             </v-card-text>
           </v-card>
         </v-col>
+        <v-col cols="12" xs="12" sm="6" md="6">
+          <v-card class="card-container">
+            <v-card-title>
+              <span class="headline">Spare parts</span>
+            </v-card-title>
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+              accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+              sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+              rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+              amet.
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </v-app>
