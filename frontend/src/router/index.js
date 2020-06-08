f75df2189b0f1616d096b5dae57f7915a9cb0558
smeisen <meisenbacher.stefan@web.de>
2020-06-08 22:45:24 +0200

    implemented authentication functionality

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,31 +4,58 @@ import VueMeta from 'vue-meta';
 import Dashboard from '../components/Dashboard.vue';
 import Maintenance from '../components/Maintenance.vue';
 import History from '../components/History.vue';
+import Login from '../components/Login.vue';
+import store from '../store';
 
 Vue.use(VueRouter);
 Vue.use(VueMeta, {
   refreshOnceOnNavigation: true,
 });
 
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+  } else {
+    next('/dashboard');
+  }
+};
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+  } else {
+    next('/login');
+  }
+};
+
+
 export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/dashboard',
+      path: '/login',
       alias: '/',
+      name: 'Login',
+      component: Login,
+      beforeEnter: ifNotAuthenticated,
+    },
+    {
+      path: '/dashboard',
       name: 'Dashboard',
       component: Dashboard,
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/maintenance',
       name: 'Maintenance',
       component: Maintenance,
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/history',
       name: 'History',
       component: History,
+      beforeEnter: ifAuthenticated,
     },
   ],
 });
