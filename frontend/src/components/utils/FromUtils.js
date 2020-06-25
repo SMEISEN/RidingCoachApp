d17028f7ea5a2cb3bae7997be75abaf5fb28a809
smeisen <meisenbacher.stefan@web.de>
2020-06-25 18:15:02 +0200

    fixed unit type bug in the form utils java script collection now it is ensured that the output type is a number

--- frontend/src/components/utils/FromUtils.js
+++ frontend/src/components/utils/FromUtils.js
@@ -1,13 +1,20 @@
 
 
 const incrementNumber = (inputNumber, increment, digits) => {
-  return Number(parseFloat(inputNumber) + increment, ).toFixed(digits);
+  return Number(Number(parseFloat(inputNumber) + increment, ).toFixed(digits));
 };
 const decrementNumber = (inputNumber, increment, digits) => {
-  return Number(parseFloat(inputNumber) - increment, ).toFixed(digits);
+  return Number(Number(parseFloat(inputNumber) - increment, ).toFixed(digits));
 };
 const initObject = (inputObject, initValue) => {
   Object.keys(inputObject).forEach((index) => inputObject[index] = initValue);
+};
+const indexOfObjectValueInArray = (arrayOfObjects, value) => {
+  for (let i = 0; i < arrayOfObjects.length; i += 1) {
+    if (Object.values(arrayOfObjects[i]).includes(value)) {
+      return i;
+    }
+  }
 };
 
 
@@ -15,4 +22,5 @@ export const FormUtils = {
   incrementNumber,
   decrementNumber,
   initObject,
+  indexOfObjectValueInArray,
 };
