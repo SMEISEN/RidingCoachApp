565302580f9ba585a9ba3749f386ba585108098e
SMEISEN <meisenbacher.stefan@web.de>
2020-07-05 16:05:55 +0200

    implemented radar chart for current setup in the dashboard page

--- frontend/src/store/modules/bike.js
+++ frontend/src/store/modules/bike.js
@@ -6,6 +6,7 @@ const state = () => ({
   current_bike_model: null,
   current_bike_operating_hours: null,
   current_bike_year: null,
+  current_bike_setup: null,
 });
 const getters = {
   getCurrentBikeId: (state) => state.current_bike_id,
@@ -13,6 +14,7 @@ const getters = {
   getCurrentBikeModel: (state) => state.current_bike_model,
   getCurrentBikeOperatingHours: (state) => state.current_bike_operating_hours,
   getCurrentBikeYear: (state) => state.current_bike_year,
+  getCurrentBikeSetup: (state) => state.current_bike_setup,
 };
 const mutations = {
   selectBike(state, selectedBike) {
@@ -21,6 +23,7 @@ const mutations = {
     state.current_bike_model = selectedBike.model;
     state.current_bike_operating_hours = selectedBike.operating_hours;
     state.current_bike_year = selectedBike.year;
+    state.current_bike_setup = selectedBike.setup;
   },
   setOperatingHours(state, operatingHours) {
     state.current_bike_operating_hours = operatingHours;
