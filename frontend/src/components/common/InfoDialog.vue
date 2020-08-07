ad1a68cc5c7675ff3d36ea305ebae61db78ad952
SMEISEN <meisenbacher.stefan@web.de>
2020-08-07 19:59:18 +0200

    added information dialog to common components

---
+++ frontend/src/components/common/InfoDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-dialog
+    v-model="infoDialog"
+    persistent
+    max-width="400"
+  >
+    <v-card>
+      <v-card-title class="headline">
+        Information
+      </v-card-title>
+      <v-card-text>
+        <p class="text--primary">
+          {{ infoText }}
+        </p>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="accent"
+          text
+          @click.prevent="onOkay"
+        >
+          OK
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'InfoDialog',
+  props: {
+    infoDialog: {
+      type: Boolean,
+      required: true,
+    },
+    infoText: {
+      type: String,
+      required: true,
+    },
+  },
+  updated() {
+  },
+  created() {
+  },
+  methods: {
+    onOkay() {
+      this.$emit('cancelButtonClicked');
+      this.$emit('update:infoDialog', false);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
