b18bb93e2e478b566b810c7ca76003f4f7c66da4
smeisen <meisenbacher.stefan@web.de>
2020-11-28 20:23:13 +0100

    closes #34

--- backend/database/models/sparepart.py
+++ backend/database/models/sparepart.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from backend.database import db, ma
+from backend.database.models.sparepartitem import SparepartitemModel
 from sqlalchemy import select, func
 from sqlalchemy.dialects.postgresql import UUID, JSON
-from sqlalchemy.orm import column_property
 import uuid
 
 
@@ -12,14 +12,14 @@ class SparepartModel(db.Model):
     sparepart_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
 
     module = db.Column(db.String, nullable=False)
-    stock = column_property(
-        select([func.count(SparepartitemModel.sparepartitem_id)])
+    min_stock = db.Column(db.Integer, nullable=True)
+
+    items = db.relationship('SparepartitemModel', backref=db.backref('sparepart'))
+    stock = db.column_property(
+        db.select([func.count(SparepartitemModel.sparepartitem_id)])
             .where(SparepartitemModel.sparepart_id == sparepart_id)
             .correlate_except(SparepartitemModel)
     )
-    min_stock = db.Column(db.Integer, nullable=True)
-
-    sparepart_item = db.relationship('SparepartitemModel', backref=db.backref('sparepart'))
 
     datetime_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     datetime_last_modified = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -34,35 +34,7 @@ class SparepartModel(db.Model):
                f")]"
 
 
-class SparepartitemModel(db.Model):
-    __tablename__ = 'sparepartitem'
-
-    sparepartitem_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    sparepart_id = db.Column(UUID(as_uuid=True), db.ForeignKey('sparepart.sparepart_id'), nullable=False)
-
-    condition = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-
-    datetime_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    datetime_last_modified = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    datetime_display = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"Sparepartitem[" \
-               f"'{self.sparepartitem_id}': (" \
-               f"'{self.condition}', " \
-               f"'{self.description}', " \
-               f"'{self.datetime_display}'" \
-               f")]"
-
-
 class SparepartSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = SparepartModel
-        include_fk = True
-
-
-class SparepartitemSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = SparepartitemModel
         include_fk = True
---
+++ backend/database/models/sparepartitem.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from backend.database import db, ma
+from sqlalchemy.dialects.postgresql import UUID, JSON
+import uuid
+
+
+class SparepartitemModel(db.Model):
+    __tablename__ = 'sparepartitem'
+
+    sparepartitem_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    sparepart_id = db.Column(UUID(as_uuid=True), db.ForeignKey('sparepart.sparepart_id'), nullable=False)
+
+    condition = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    datetime_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    datetime_last_modified = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    datetime_display = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+            return f"Sparepartitem[" \
+                   f"'{self.sparepartitem_id}': (" \
+                   f"'{self.condition}', " \
+                   f"'{self.description}', " \
+                   f"'{self.datetime_display}'" \
+                   f")]"
+
+
+class SparepartitemSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = SparepartitemModel
+        include_fk = True
