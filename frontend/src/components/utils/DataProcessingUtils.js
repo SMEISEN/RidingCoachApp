af9be8a123bc9cb57ab2d5d99a556ae6f12db2b1
SMEISEN <meisenbacher.stefan@web.de>
2021-01-03 12:49:23 +0100

    [#62] removed obsolete maintenance state functions

--- frontend/src/components/utils/DataProcessingUtils.js
+++ frontend/src/components/utils/DataProcessingUtils.js
@@ -9,36 +9,6 @@ export const flattenNestedObjects = (inputObject) => {
   }
   return outputArray;
 };
-export const processStateOfIntervalHours = (latest, interval, current, digits) => {
-  const state = ((latest + interval - current) / interval) * 100;
-  return Number.parseFloat(state.toPrecision(digits));
-};
-export const processLeftIntervalHours = (latest, interval, current, digits) => {
-  const hoursLeft = latest + interval - current;
-  return Number.parseFloat(hoursLeft.toPrecision(digits));
-};
-export const processLeftIntervalYears = (latestDate, digits) => {
-  const latest = new Date(latestDate);
-  const now = new Date();
-  const start = new Date(latest.getFullYear(), 0, 0);
-  const diff = now - start;
-  const oneDay = 1000 * 60 * 60 * 24;
-  const day = Math.floor(diff / oneDay);
-  const daysLeft = 365 - day;
-
-  return Number.parseFloat(daysLeft.toPrecision(digits));
-};
-export const processStateOfIntervalYears = (latestDate, digits) => {
-  const latest = new Date(latestDate);
-  const now = new Date();
-  const start = new Date(latest.getFullYear(), 0, 0);
-  const diff = now - start;
-  const oneDay = 1000 * 60 * 60 * 24;
-  const day = Math.floor(diff / oneDay);
-  const state = (day / 365) * 100;
-
-  return Number.parseFloat(state.toPrecision(digits));
-};
 export const interpolateLinearly1D = (targetX, arrayXY) => {
   if (arrayXY.length === 0) {
     throw RangeError('Empty array was given!');
