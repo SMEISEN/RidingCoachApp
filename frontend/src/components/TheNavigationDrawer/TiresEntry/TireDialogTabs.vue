8781ee1713a1572b4dbae58a8f26ca59087b0887
SMEISEN <meisenbacher.stefan@web.de>
2021-10-03 19:09:34 +0200

    [#115] restrucutred tire dialog base

--- frontend/src/components/TheNavigationDrawer/TiresEntry/TireDialogTabs.vue
+++ frontend/src/components/TheNavigationDrawer/TiresEntry/TireDialogTabs.vue
@@ -1,67 +1,34 @@
 <template>
-    <div>
-        <v-tabs
-            fixed-tabs
-            background-color="secondary"
-            dark
-        >
-            <v-tab>
-                Slick
-            </v-tab>
-            <v-tab>
-                Rain
-            </v-tab>
-        </v-tabs>
-        <v-expansion-panels
-            v-model="tire_panel"
-            multiple
-        >
-            <v-expansion-panel>
-            <v-expansion-panel-header>Front</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                <v-container>
-                </v-container>
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-            <v-expansion-panel>
-            <v-expansion-panel-header>Rear</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                <v-container>
-                </v-container>
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
-    </div>
+  <v-tabs
+    fixed-tabs
+    background-color="secondary"
+    dark
+  >
+    <v-tab>
+      Slick
+    </v-tab>
+    <v-tab-item>
+      <TireDialogTabsExpansion />
+    </v-tab-item>
+    <v-tab>
+      Rain
+    </v-tab>
+    <v-tab-item>
+      <TireDialogTabsExpansion />
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script>
+import TireDialogTabsExpansion from './TireDialogTabsExpansion.vue';
 
 export default {
   name: 'TireDialogTabs',
   components: {
+    TireDialogTabsExpansion
   },
   data: () => ({
-    tire_panel: [0,1],
   }),
-  computed: {
-    tire_dialog: {
-        get() {
-        return this.$store.getters.getTireDialogState;
-        },
-        set(value) {
-        this.$store.commit('setTireDialogState', value);
-        },
-    },
-  },
-  updated() {
-    this.window_height = window.innerHeight;
-  },
-  methods: {
-      onTireCancel() {
-      this.tire_dialog = false;
-      this.$refs.validation_tire_form.resetValidation();
-    },
-  },
 };
 </script>
 
---
+++ frontend/src/components/TheNavigationDrawer/TiresEntry/TireDialogTabsExpansion.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-expansion-panels
+    v-model="tire_panel"
+    multiple
+  >
+    <v-expansion-panel>
+      <v-expansion-panel-header>Front</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-container>
+          </v-container>
+        </v-expansion-panel-content>
+    </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Rear</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-container>
+            </v-container>
+          </v-expansion-panel-content>
+      </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+
+export default {
+  name: 'TireDialogTabsExpansion',
+  components: {
+  },
+  data: () => ({
+    tire_panel: [0,1],
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
