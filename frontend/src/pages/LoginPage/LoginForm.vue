d188906b4224c42b9c623ffcbb29bf110cc6edfd
SMEISEN <meisenbacher.stefan@web.de>
2021-12-24 13:01:19 +0100

    [#125] docstrings for the login page

--- frontend/src/pages/LoginPage/LoginForm.vue
+++ frontend/src/pages/LoginPage/LoginForm.vue
@@ -91,6 +91,9 @@ export default {
     },
   },
   methods: {
+    /**
+     * Emits the username and password to the parent page and sets loading animation to true.
+     */
     onLoginButton() {
       this.$emit('update:loading', true);
       this.$emit('loginButtonClicked', this.username, this.password);
