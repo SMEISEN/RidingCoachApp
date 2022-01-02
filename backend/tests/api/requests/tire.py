675b39c196afdd37b0f66ab23eef17a9d04b03ff
SMEISEN <meisenbacher.stefan@web.de>
2022-01-02 14:19:55 +0100

    [#127] added tests for the tire api

---
+++ backend/tests/api/requests/tire.py
@@ -0,0 +1,84 @@
+import json
+from backend.tests.api.requests.bike import post as post_bike
+
+default_payload_post = {
+    "active": False,
+    "rim": "Alpina black",
+    "category": "slick",
+    "manufacturer": "Metzeler",
+    "name": "Metzeler",
+    "compound": "K1",
+    "axis": "front",
+    "dimension": "125/75R420",
+    "dot": "1521",
+    "condition": {
+        "left_outer": 1,
+        "left_middle": 1,
+        "center": 1,
+        "right_middle": 1,
+        "right_outer": 1
+    },
+    "operating_hours": 0,
+    "comment": "comment on the tire"
+}
+
+default_payload_put = {
+    "active": True,
+    "rim": "Alpina white",
+    "category": "rain",
+    "manufacturer": "Michelin",
+    "name": "Michelin",
+    "compound": "B2",
+    "axis": "rear",
+    "dimension": "160/60R17",
+    "dot": "1621",
+    "condition": {
+        "left_outer": 0.95,
+        "left_middle": 0.94,
+        "center": 0.93,
+        "right_middle": 0.92,
+        "right_outer": 0.91
+    },
+    "operating_hours": 1.2,
+    "comment": "comment"
+}
+
+def get(app, client):
+    response = client.get("/api/tire", headers={"apikey": app.config['FLASK_RESTPLUS_API_KEY']})
+
+    return response
+
+
+def post(app, client, payload=None):
+    if payload is None:
+        payload = default_payload_post
+
+    # post a bike first
+    response = post_bike(app, client)
+    payload["bike_id"] = json.loads(response.get_data())
+
+    response = client.post("/api/tire", data=json.dumps(payload), content_type='application/json',
+                           headers={"apikey": app.config['FLASK_RESTPLUS_API_KEY']})
+
+    return response
+
+
+def get_item(app, client, id_):
+    response = client.get(f"/api/tire/{id_}", headers={"apikey": app.config['FLASK_RESTPLUS_API_KEY']})
+
+    return response
+
+
+def put_item(app, client, id_, payload=None):
+    if payload is None:
+        payload = default_payload_put
+    response = client.put(f"/api/tire/{id_}", data=json.dumps(payload), content_type='application/json',
+                          headers={"apikey": app.config['FLASK_RESTPLUS_API_KEY']})
+
+    return response
+
+
+def delete_item(app, client, id_):
+    response = client.delete(f"/api/tire/{id_}", headers={"apikey": app.config['FLASK_RESTPLUS_API_KEY']})
+
+    return response
---
+++ backend/tests/api/test_tire.py
@@ -0,0 +1,97 @@
+import json
+import pytest
+from backend.app import create_app
+from backend.config import TestConfig
+from backend.app import db
+from backend.utils.uuid import validate_uuid
+
+from backend.tests.api.requests.tire import get
+from backend.tests.api.requests.tire import post
+from backend.tests.api.requests.tire import get_item
+from backend.tests.api.requests.tire import put_item
+from backend.tests.api.requests.tire import delete_item
+from backend.tests.api.requests.tire import default_payload_post, default_payload_put
+
+
+@pytest.fixture
+def app():
+    # setup
+    _app = create_app(TestConfig)
+
+    # create test tables
+    with _app.app_context():
+        db.drop_all()
+        db.create_all()
+
+    yield _app
+
+    # teardown
+    with _app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    _client = app.test_client()
+    return _client
+
+
+@pytest.fixture
+def tire_id(app, client):
+    response = get(app, client)
+
+    if bool(json.loads(response.get_data())) is False:
+        # create a new tire item
+        response = post(app, client)
+        tire_id_ = json.loads(response.get_data())
+    else:
+        # extract id from existing tire item
+        tire_id_ = response.get_data()[0]["tire_id"]
+    return tire_id_
+
+
+def test_get(app, client):
+    # GET /tire/
+    response = get(app, client)
+    assert bool(json.loads(response.get_data())) is False  # response must be empty
+    assert response.status_code == 200
+
+
+def test_post(app, client):
+    # POST /tire/
+    response = post(app, client)
+    tire_id = json.loads(response.get_data())
+    assert validate_uuid(tire_id, 4) is True  # response must be a valid uuid4
+    assert response.status_code == 201
+
+
+def test_get_item(app, client, tire_id):
+    # GET /tire/{id_}
+    response = get_item(app, client, tire_id)
+    tire_item = json.loads(response.get_data())
+    assert tire_item["tire_id"] == tire_id
+    assert validate_uuid(tire_item["tire_id"], 4) is True
+    for key, value in default_payload_post.items():
+        assert tire_item[key] == value
+    assert response.status_code == 200
+
+
+def test_put_item(app, client, tire_id):
+    # PUT /tire/{id_}
+    response = put_item(app, client, tire_id)
+    assert response.status_code == 204
+
+    response = get_item(app, client, tire_id)
+    tire_item = json.loads(response.get_data())
+    assert tire_item["tire_id"] == tire_id
+    assert validate_uuid(tire_item["tire_id"], 4) is True
+    for key, value in default_payload_put.items():
+        assert tire_item[key] == value
+
+
+def test_delete_item(app, client, tire_id):
+    # DELETE /tire/{id_}
+    response = delete_item(app, client, tire_id)
+    assert response.status_code == 204
+    response = get(app, client)
+    assert bool(json.loads(response.get_data())) is False  # response must be empty
