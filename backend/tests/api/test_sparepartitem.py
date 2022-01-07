decdb2ac4b02e8f1b82a5849783bd787a02b9f5e
SMEISEN <meisenbacher.stefan@web.de>
2022-01-07 15:34:10 +0100

    [#127] extended test for the get request

--- backend/tests/api/test_sparepartitem.py
+++ backend/tests/api/test_sparepartitem.py
@@ -53,6 +53,16 @@ def test_get(app, client):
     assert bool(json.loads(response.get_data())) is False  # response must be empty
     assert response.status_code == 200
 
+    # post two tire items
+    post(app, client, default_payload_post)
+    post(app, client, default_payload_put)
+
+    response = get(app, client)
+    tire_items = json.loads(response.get_data())
+    assert isinstance(tire_items, list)
+    assert len(tire_items) == 2
+    assert response.status_code == 200
+
 
 def test_post(app, client):
     # POST /sparepartitem/
